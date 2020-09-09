d0f2c5077b06f7b58a88631e0dd9a8158fcff087
Chandula <chandula.madigasekara@gmail.com>
2020-09-09 10:25:50 +0530

    First commit

---
+++ app/src/main/java/com/example/puppypals_foryourpooch/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.puppypals_foryourpooch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
