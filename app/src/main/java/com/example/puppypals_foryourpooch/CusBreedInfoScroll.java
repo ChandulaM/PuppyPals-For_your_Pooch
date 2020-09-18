d920f6b1ee69be610dbf267fd6c2ab7557e17fce
Nadun <dilhannadun777@gmail.com>
2020-09-18 23:07:12 +0530

    scroll

---
+++ app/src/main/java/com/example/puppypals_foryourpooch/CusBreedInfoScroll.java
@@ -0,0 +1,39 @@
+package com.example.puppypals_foryourpooch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+public class CusBreedInfoScroll extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cus_breed_info_scroll);
+    }
+
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.popup_menu);
+        popup.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.pitm1:
+                Toast.makeText(this,"Item 1 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.pitm2:
+                Toast.makeText(this,"Item 2 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return false;
+    }
+}
