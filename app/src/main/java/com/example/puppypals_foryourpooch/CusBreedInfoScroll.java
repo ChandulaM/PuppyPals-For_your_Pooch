8d2eba8d5da369b69421f9b7df14f0c9829e77af
Nadun <dilhannadun777@gmail.com>
2020-09-24 12:39:23 +0530

    UI fix

--- app/src/main/java/com/example/puppypals_foryourpooch/CusBreedInfoScroll.java
+++ app/src/main/java/com/example/puppypals_foryourpooch/CusBreedInfoScroll.java
@@ -2,6 +2,8 @@ package com.example.puppypals_foryourpooch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,7 +24,7 @@ public class CusBreedInfoScroll extends AppCompatActivity implements PopupMenu.O
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cus_breed_info_scroll);
-        BreedModel model = (BreedModel) getIntent().getSerializableExtra("Breed");
+        final BreedModel model = (BreedModel) getIntent().getSerializableExtra("Breed");
 
         im = findViewById(R.id.cusBreedimg);
         brdName = findViewById(R.id.cusBreednm);
@@ -48,6 +50,15 @@ public class CusBreedInfoScroll extends AppCompatActivity implements PopupMenu.O
         intell.setText(model.getIntelligence());
         feed.setText(model.getFeedings());
         hlth.setText(model.getHealth());
+
+        morInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse(model.getLink());
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
 
 
     }
